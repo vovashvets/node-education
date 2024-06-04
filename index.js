805809bc75c7495dfede9f8170c3e1fc3f27c43c
Volodymyr Shvets <vova.shvets@ekreative.com>
2024-06-04 10:55:12 +0300

    yargs lib

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import chalk from "chalk";
+import yargs from "yargs";
+import {hideBin} from "yargs/helpers";
 
-const args = process.argv[2];
-
-if (args === "add") {
+// node index.js add-test
+if (process.argv[2] === "add-test") {
   console.log(chalk.blue.bold("Add note"));
-} else if (args === "delete") {
-  console.log(chalk.blue.bold("Delete note"));
 }
+
+// node index.js add --title="My title"
+yargs(hideBin(process.argv))
+    .command('add', 'Add a new note', () => {
+      console.log("Adding a new note")
+    }, (argv) => {
+      console.info(`Note title is:${argv.title}`)
+    })
+    .option('title', {
+      alias: 't',
+      type: 'string',
+      description: 'Note title'
+    })
+    .parse()
